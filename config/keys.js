ab2a5f02bb8fb7c42f3cd8090eb24515471050d4
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-12-13 23:58:46 +0900

    habesha-freelancer_First-ever

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://habesha_freelancer:habesha@habeshafreelancer-clust.giqt1rt.mongodb.net/?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
---
+++ models/Profile.js
@@ -0,0 +1,120 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId, // Associate user by id
+    ref: 'users'// Collection referred to in the database
+  },
+  handle: {//Profile handle
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {// Career status
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String], // Put comma separated values
+    required: true
+  },
+  bio: {//Self-description
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [//Array of experiences
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [//Array of educational information
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
